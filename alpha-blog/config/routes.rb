4ec776588ca1fa9778dfc7be8787c05359dc05b7
Rick Thornbrugh <rthornbrug@gmail.com>
2016-11-19 07:38:33 +0000

    add new article controller, and view template with form.

--- alpha-blog/config/routes.rb
+++ alpha-blog/config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   root 'page#home'
   get 'about', to: 'page#about'
+  
+  resources :articles
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
