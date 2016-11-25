5149477da001431077c1ac8e2e74d9d2d6d9a22e
Rick Thornbrugh <rthornbrug@gmail.com>
2016-11-25 21:08:35 +0000

    fix create, add flash, and add show fuctionality to app.

--- alpha-blog/app/controllers/articles_controller.rb
+++ alpha-blog/app/controllers/articles_controller.rb
@@ -5,12 +5,20 @@ class ArticlesController < ApplicationController
   
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
-  end  
+    if @article.save
+      flash[:notice] = "Article was successfuly created."
+      redirect_to article_path(@article)
+    else
+      render "new"
+    end  
+  end
   
+  def show
+    @article = Article.find(params[:id])
+  end  
+
   private
    def article_params
      params.require(:article).permit(:title, :description)
    end
-end  
+end
