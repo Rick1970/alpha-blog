3e259aadada090a8c22812788555a9e7053e2190
Rick Thornbrugh <rthornbrug@gmail.com>
2016-11-29 23:02:51 +0000

    add error styling, and flash styling.

--- alpha-blog/app/controllers/articles_controller.rb
+++ alpha-blog/app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article was successfuly created."
+      flash[:success] = "Article was successfuly created."
       redirect_to article_path(@article)
     else
       render "new"
@@ -27,13 +27,13 @@ class ArticlesController < ApplicationController
   
   def destroy
     @article.destroy
-    flash[:notice] = "Article was successfuly deleted"
+    flash[:danger] = "Article was successfuly deleted"
     redirect_to articles_path
   end  
   
   def update
     if @article.update(article_params)
-      flash[:notice] = "Article was successfuly updated."
+      flash[:success] = "Article was successfuly updated."
       redirect_to article_path(@article)
     else
       render 'edit'
