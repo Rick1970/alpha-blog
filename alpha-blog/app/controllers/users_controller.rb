b2d37e68c35d52bf701625a3f2d144ad37df4a94
Rick Thornbrugh <rthornbrug@gmail.com>
2016-12-06 22:01:55 +0000

    finish admin functionality.

--- alpha-blog/app/controllers/users_controller.rb
+++ alpha-blog/app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   
-  before_action :set_user, only:[:edit, :update, :show]
-  before_action :require_same_user, only:[:edit, :update]
+  before_action :set_user, only:[:edit, :update, :show, :destroy]
+  before_action :require_same_user, only:[:edit, :update, :destroy]
+  before_action :require_admin, only:[:destroy]
   
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -25,8 +26,13 @@ class UsersController < ApplicationController
   
   def show
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
-    
   end   
+  
+  def destroy
+    @user.destroy
+    flash[:danger] = "User and all their articles have been deleted."
+    redirect_to users_path
+  end  
   
     def edit 
     end  
@@ -51,8 +57,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end  
   
+  def require_admin
+    if logged_in? and !current_user.admin?
+      flash[:danger] = "Only admin users can perform that action."
+      redirect_to root_path
+    end  
+  end  
+  
   def require_same_user
-    if current_user != @user
+    if current_user != @user and !current_user.admin?
       flash[:danger] = "You an only edit your own account."
       redirect_to root_path
     end  
