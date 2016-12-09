4380166d784984e6af1fbe737bd32600459ac5fa
Rick Thornbrugh <rthornbrug@gmail.com>
2016-12-09 22:32:07 +0000

    add category class, and table with automated testing included.

---
+++ alpha-blog/test/models/category_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+class CategoryTest < ActiveSupport::TestCase
+  
+  def setup
+    @category = Category.new(name: "sports")
+  end  
+  
+  test "Category should be valid." do
+    assert @category.valid?
+  end
+  
+  test "Name should be present." do
+    @category.name = " "
+    assert_not @category.valid?
+  end  
+  
+  test "Name should be unique." do
+    @category.save 
+    category2 = Category.new(name: "sports")
+    assert_not category2.valid?
+  end  
+  
+  test "Name should not be to long." do
+    @category.name = "a" * 26
+    assert_not @category.valid?
+  end  
+  
+  test "Name should not be to short." do
+    @category.name = "aa"
+    assert_not @category.valid?
+  end
+  
+  
+end  
