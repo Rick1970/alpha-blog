73d09531bdc67976109afc9c3d98f20874fd6897
Rick Thornbrugh <rthornbrug@gmail.com>
2016-12-14 22:44:39 +0000

    add article, and category association.

--- alpha-blog/db/schema.rb
+++ alpha-blog/db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161209220139) do
+ActiveRecord::Schema.define(version: 20161214203448) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
