fb4c2e94553c8404d84774017d1331dbd127995d
Rick Thornbrugh <rthornbrug@gmail.com>
2016-12-14 18:39:50 +0000

    add list category integration testing.

---
+++ alpha-blog/test/integration/list_categories_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class ListCategoriesTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @category = Category.create(name: "sports")
+    @category2 = Category.create(name: "programming")
+  end  
+  
+  test "Should show category listing" do
+    get categories_path
+    assert_template 'categories/index'
+    assert_select "a[href=?]", category_path(@category), text: @category.name
+    assert_select "a[href=?]", category_path(@category2), text: @category2.name
+    
+  end
+    
+end 
