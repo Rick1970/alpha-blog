2d94932d16df77d7ba65b526aac3499825b16866
Rick Thornbrugh <rthornbrug@gmail.com>
2016-12-15 21:39:26 +0000

    add category edit feature.

--- alpha-blog/app/controllers/categories_controller.rb
+++ alpha-blog/app/controllers/categories_controller.rb
@@ -24,12 +24,25 @@ class CategoriesController < ApplicationController
     @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end  
   
+  def edit 
+    @category = Category.find(params[:id])
+  end  
+  
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = "Category name was successfully updated."
+      redirect_to category_path(@category)
+    else
+     render "edit"
+    end 
+  end
   private
   
   def require_admin
     if !logged_in? || (logged_in? and !current_user.admin?)
       flash[:danger] = "Only admins can perform that action."
-      redirect_to categories_path
+      redirect_to category_path
     end  
   end  
   
